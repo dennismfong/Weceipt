6f9dc0b93261979433c6a9af73b6e99d18a7230d
Jacky Zheng <jzheng1126@yahoo.com>
2018-06-13 15:13:11 -0700

    Crap

---
+++ front-end/app/src/main/java/weceipt/ece150/com/weceipt/FinalPrices.java
@@ -0,0 +1,34 @@
+package weceipt.ece150.com.weceipt;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class FinalPrices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_final_prices);
+
+        TextView first = (TextView) findViewById(R.id.finalPrice1);
+        TextView second = (TextView) findViewById(R.id.finalPrice2);
+        TextView third = (TextView) findViewById(R.id.finalPrice3);
+        TextView fourth = (TextView) findViewById(R.id.finalPrice4);
+
+        Intent intent = getIntent();
+        ArrayList<Integer> prices = (ArrayList<Integer>) intent.getSerializableExtra("data");
+
+        first.setText("Final Price: " + prices.get(0));
+        second.setText("Final Price: " + prices.get(1));
+        third.setText("Final Price: " + prices.get(2));
+        fourth.setText("Final Price: " + prices.get(3));
+        }
+}
